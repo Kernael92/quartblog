ecc8ca01311bdcfbe51480b30e02e100861cf4dd
Kernael92 <kernaelapuko5@gmail.com>
2019-09-05 22:41:42 +0300

    add new post to the app creation function

--- main/__init__.py
+++ main/__init__.py
@@ -5,6 +5,9 @@ async def create_app(config):
     app = Quart(__name__)
     DB.init()
     register_blueprints(app)
+    for title, text in ['post1', 'post2', 'post3']:
+        new_post = Post(title=title, text=text)
+        new_post.insert()
     return app 
 
 async def register_blueprints(app):
